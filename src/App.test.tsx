2cea0bdd116beeaf0b2d6f2f0419d42ff549f7a6
Yogesh Dhamija <ydhamija96@gmail.com>
2020-05-23 21:19:57 -0400

    create debate component skeleton

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should load the debate component', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText(/debate/i)).toBeInTheDocument();
 });
---
+++ src/Debate.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Debate extends React.Component {
+    render() {
+        return "Debate";
+    }
+}
