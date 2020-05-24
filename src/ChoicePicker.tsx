6b72af9f6c2f29ed496b2c9506f1293678c2c4f1
Yogesh Dhamija <ydhamija96@gmail.com>
2020-05-24 18:30:51 -0400

    better looking buttons

--- src/ChoicePicker.tsx
+++ src/ChoicePicker.tsx
@@ -12,7 +12,11 @@ export class ChoicePicker extends React.Component<ChoicePickerProps> {
         let choiceElements : JSX.Element[] = [];
 
         this.props.choices.forEach((choice) => {
-            choiceElements.push(<Button variant="outline-dark" size="sm" key={choice.id}>{choice.text}</Button>);
+            choiceElements.push(
+                <Button variant="light" size="sm" key={choice.id}>
+                    {choice.text}
+                </Button>
+            );
         })
 
         return (
